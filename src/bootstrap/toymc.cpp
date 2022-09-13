91855849378058503d0fbb9dd0c274a5c52d03c9
Zelong Zhang <zelong.zhang@icecube.wisc.edu>
2022-09-12 19:45:06 -0500

    updated to suit with new systematic parameters, clean the white space at the same time

--- src/bootstrap/toymc.cpp
+++ src/bootstrap/toymc.cpp
@@ -9,12 +9,11 @@ std::vector<TH3D *> NuFit::toymc::draw_sample(std::map<std::string, double> &par
 	for (unsigned int i=0; i<hists_mcsum.size(); ++i)
 	{
 		TH3D *hist = hists_mcsum[i];
-                // store original mcsum
-                TH3D *hist_clone = (TH3D*) hist -> Clone();
-                std::string name = std::string(hist_clone->GetName(), 0, 1000);
-                hist_clone->SetName((std::string(name)+std::string("_")+std::string("orig")).c_str());
-                hists_mcsum_orig.push_back(hist_clone);
-
+        // store original mcsum
+        TH3D *hist_clone = (TH3D*) hist -> Clone();
+        std::string name = std::string(hist_clone->GetName(), 0, 1000);
+        hist_clone->SetName((std::string(name)+std::string("_")+std::string("orig")).c_str());
+        hists_mcsum_orig.push_back(hist_clone);
 		for (int k=0; k<hist->GetNbinsX(); ++k)
 		{
 			for (int l=0; l<hist->GetNbinsY(); ++l)
@@ -29,18 +28,14 @@ std::vector<TH3D *> NuFit::toymc::draw_sample(std::map<std::string, double> &par
 				}
 			}
 		}
-		
 	}
-
-        std::vector<TH3D *> hists;
-        for(unsigned int i=0; i<hists_mcsum.size(); ++i)
-        {
-                hists.push_back(hists_mcsum[i]);
-                hists.push_back(hists_mcsum_orig[i]);
+    std::vector<TH3D *> hists;
+    for(unsigned int i=0; i<hists_mcsum.size(); ++i)
+    {
+        hists.push_back(hists_mcsum[i]);
+        hists.push_back(hists_mcsum_orig[i]);
 	}
-
 	return hists;
-
 }
 
 void NuFit::toymc::run_simulation(std::string outfile, std::map<std::string, double> simpars, unsigned int nsamples)
@@ -68,66 +63,67 @@ void NuFit::toymc::run_simulation(std::string outfile, std::map<std::string, dou
 }
 
 void NuFit::toymc::run_simulation_waux(std::string outfile, std::string outfile_waux, std::map<std::string, double> simpars, unsigned int nsamples) {
-        std::cout << "\n";
-        std::cout << "... sampling " << nsamples << " toymc datasets for fixed set of model parameters." << std::endl;
-        TFile fout(outfile.c_str(), "RECREATE");
-		
+    std::cout << "\n";
+    std::cout << "... sampling " << nsamples << " toymc datasets for fixed set of model parameters." << std::endl;
+    TFile fout(outfile.c_str(), "RECREATE");
+	
 	std::ofstream file;
 	file.open(outfile_waux);
 	file.precision(5);
 	file.setf(std::ios_base::scientific);
 
-	file << "dom_efficiency" << "\t" << "delta_cr" << "\t" << "absorption" << "\t" << "scattering" << "\t" << "zholeice_scattering" << "\n";	
+	file << "dom_efficiency" << "\t" << "delta_cr" << "\t" << "absorption" << "\t" << "scattering" << "\t" << "holeicep0" << "\t" << "holeicep1" << "\t" << "selfveto" << "\t" << "hadronicinteraction" << "\n";	
 	//file << "dom_efficiency" << "\t" << "delta_cr" << "\t" << "absorption" << "\t" << "scattering" << "\t" <<  "muon_norm_mlb" << "\n";
-        //file << "dom_efficiency" << "\t" << "delta_cr" << "\n";
+    //file << "dom_efficiency" << "\t" << "delta_cr" << "\n";
 
 	double means_simulated[2] = {simpars["absorption"], simpars["scattering"]};
 	double domeff_simulated = simpars["dom_efficiency"];
-	double holeice_simulated = simpars["zholeice_scattering"];
 	double deltacr_simulated = simpars["delta_cr"];
-//	double muon_norm_mlb_simulated = simpars["muon_norm_mlb"];
+	double holeicep0_simulated = simpars["holeicep0"];
+	double holeicep1_simulated = simpars["holeicep1"];
+    double selfveto_simulated = simpars["selfveto"];
+    double hadronicinteraction_simulated = simpars["hadronicinteraction"];
 
 	double prior_domeff_sigma = 0.1;
-	double prior_hi_sigma = 0.66;
 	double prior_deltacr_sigma = 0.05; 
-//	double prior_muon_norm_mlb_sigma = 0.38;
-        std::cout<<simpars.size();
-        for (unsigned int i=0; i<nsamples; ++i)
+	double prior_holeicep0_sigma = 1;
+	double prior_holeicep1_sigma = 0.2;
+    double prior_selfveto_sigma = 1000;
+    double prior_hadronicinteraction_sigma = 2;
+    std::cout<<simpars.size();
+    for (unsigned int i=0; i<nsamples; ++i)
+    {
+        std::vector<TH3D*> hists = draw_sample(simpars);
+        for (unsigned int j=0; j<hists.size(); ++j)
         {
-                std::vector<TH3D*> hists = draw_sample(simpars);
-                for (unsigned int j=0; j<hists.size(); ++j)
-                {
-                        // ROOT uses C-style character arrays
-                        TH3D *hist = hists[j];
-                        std::string name = std::string(hist->GetName(), 0, 1000);
-                        hist->SetName((std::string(name)+std::string("_")+std::to_string(i)).c_str());
-                        hist->Write();
-                        delete hists[j];
-                }
-
-		// update auxiliary data
-		double prior_domeff_mean_current = generate_truncated_normal_rv(domeff_simulated, prior_domeff_sigma);
-		double prior_hi_mean_current = generate_truncated_normal_rv(holeice_simulated, prior_hi_sigma);
-		double prior_deltacr_mean_current = generate_normal_rv(deltacr_simulated, prior_deltacr_sigma);
-		//double prior_muon_norm_mlb_mean_current = generate_truncated_normal_rv(muon_norm_mlb_simulated, prior_muon_norm_mlb_sigma);
-
-		
-
-		double rvars[2] = {0.0, 0.0};
-		generate_truncated_bivnorm_rv(means_simulated, rvars);
-		double prior_absorption_current = rvars[0];
-		double prior_scattering_current = rvars[1];
-
-		file << prior_domeff_mean_current << "\t" << prior_deltacr_mean_current << "\t" << prior_absorption_current << "\t" << prior_scattering_current << "\t" << prior_hi_mean_current << "\n";
-		//file << prior_domeff_mean_current << "\t" << prior_deltacr_mean_current << "\t" << prior_absorption_current << "\t" << prior_scattering_current << "\t" << prior_muon_norm_mlb_mean_current << "\n";
-		//file << prior_domeff_mean_current << "\t" << prior_deltacr_mean_current << "\n";
-	
+            // ROOT uses C-style character arrays
+            TH3D *hist = hists[j];
+            std::string name = std::string(hist->GetName(), 0, 1000);
+            hist->SetName((std::string(name)+std::string("_")+std::to_string(i)).c_str());
+            hist->Write();
+            delete hists[j];
         }
 
-        fout.Close();
+	// update auxiliary data
+	double prior_domeff_mean_current = generate_truncated_normal_rv(domeff_simulated, prior_domeff_sigma);
+	double prior_deltacr_mean_current = generate_normal_rv(deltacr_simulated, prior_deltacr_sigma);
+	double prior_holeicep0_mean_current = generate_truncated_normal_rv(holeicep0_simulated, prior_holeicep0_sigma);
+	double prior_holeicep1_mean_current = generate_truncated_normal_rv(holeicep1_simulated, prior_holeicep1_sigma);
+    double prior_selfveto_mean_current = generate_truncated_normal_rv(selfveto_simulated, prior_selfveto_sigma);
+    double prior_hadronicinteraction_mean_current = generate_truncated_normal_rv(hadronicinteraction_simulated, prior_hadronicinteraction_sigma);
+
+	double rvars[2] = {0.0, 0.0};
+	generate_truncated_bivnorm_rv(means_simulated, rvars);
+	double prior_absorption_current = rvars[0];
+	double prior_scattering_current = rvars[1];
+
+	file << prior_domeff_mean_current << "\t" << prior_deltacr_mean_current << "\t" << prior_absorption_current << "\t" << prior_scattering_current << "\t" << prior_holeicep0_mean_current << "\t" << prior_holeicep1_mean_current << "\t" << prior_selfveto_mean_current << "\t" << prior_hadronicinteraction_mean_current << "\n";
+    }
+
+    fout.Close();
 	file.close();
-        std::cout << "... done" << std::endl;
-        return;	
+    std::cout << "... done" << std::endl;
+    return;	
 }
 
 void NuFit::toymc::run_simulation_bayesian(std::string outfile, std::map<std::string, double> simpars_fixed, std::vector<std::string> simpars_float, std::string infile_parameter_samples, unsigned int nsamples)
